d80cbab0ed3a72bec0038979431db0e5111c73b5
Zhilin Wang <wangzhilin123@gmail.com>
2024-01-15 15:22:51 -0800

    support openai v1 python client

--- humanoidagents/llm.py
+++ humanoidagents/llm.py
@@ -1,46 +1,52 @@
 import openai
+from openai import OpenAI
+
 from functools import cache
 
+
+
 class OpenAILLM:
-    @staticmethod
-    def get_llm_response(prompt, max_tokens=None, timeout=60):
+
+    client = OpenAI()
+    
+    @classmethod
+    def get_llm_response(cls, prompt, max_tokens=None, timeout=60):
         n_retries = 10
         for i in range(n_retries):
             try:
                 if isinstance(max_tokens, int):
-                    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}], max_tokens=max_tokens, request_timeout=timeout)
+                    chat_completion = cls.client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}], max_tokens=max_tokens, timeout=timeout)
                 else:
-                    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}], request_timeout=timeout)
+                    chat_completion = cls.client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}], timeout=timeout)
                 return chat_completion.choices[0].message.content
-            except openai.error.ServiceUnavailableError:
+            except openai.APIError:
                 print("openai.error.ServiceUnavailableError")
                 pass
-            except openai.error.Timeout:
+            except openai.APITimeoutError:
                 print("openai.error.Timeout")
                 pass
-            except openai.error.APIError:
+            except openai.APIError:
                 print("openai.error.APIError")
                 pass
-            except openai.error.APIConnectionError:
+            except openai.APIConnectionError:
                 pass
-            except openai.error.RateLimitError:
+            except openai.RateLimitError:
                 print("openai.error.RateLimitError")
                 time.sleep(10)
             # too many tokens
-            except openai.error.InvalidRequestError:
+            except openai.BadRequestError:
                 context_window = 3000 * 4 # max length in chars (every token is around 4 chars)
                 prompt = prompt[:context_window]
                 print("openai.error.InvalidRequestError")
 
         raise ValueError(f"OpenAI remains uncontactable after {n_retries} retries due to either Server Error or Timeout after {timeout} seconds")
 
-    @cache
     @staticmethod
+    @cache
     def get_embeddings(query):
-        response = openai.Embedding.create(
+        response = openai.embeddings.create(
             input=query,
             model="text-embedding-ada-002"
         )
-        embeddings = response['data'][0]['embedding']
+        embeddings = response.data[0].embedding
         return embeddings
-    
